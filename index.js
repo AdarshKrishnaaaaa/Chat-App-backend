63654e18494a023a11291167841e7af6e0798d87
Adarsh <adarsh66666@gmail.com>
2026-08-07 17:11:33 +0530

    Update origin changes.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
